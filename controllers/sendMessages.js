9e12d3c4b97e76e8df878c7e43ae59836c899d73
Emad Khatrush <qwe.emad@hotmail.com>
2022-10-24 12:42:47 +0300

    generate QR code

--- controllers/sendMessages.js
+++ controllers/sendMessages.js
@@ -5,13 +5,30 @@ const ErrorHandler = require('../utils/errorHandler');
 module.exports.sendWhatsupMessage = async (req, res, next) => {
   const { phone, message } = req.body;
   // check for Qr Code
-  wbm.start()
+  // wbm.start()
+  //   .then(async (qrCodeData) => {
+  //     const receiver = validatePhoneNumber(phone);
+  //     const phones = [receiver];
+  //     await wbm.send(phones, message);
+  //     await wbm.end();
+  //     res.status(200).send(qrCodeData)
+  //   })
+  //   .catch(error => res.send(error))
+
+    // generete
+    wbm
+    .start({ qrCodeData: true, session: false, showBrowser: false })
     .then(async (qrCodeData) => {
-      const receiver = validatePhoneNumber(phone);
-      const phones = [receiver];
+      console.log(qrCodeData); // show data used to generate QR Code
+      res.send(qrCodeData);
+      await wbm.waitQRCode();
+
+      const phones = [phone];
+
       await wbm.send(phones, message);
       await wbm.end();
-      res.status(200).send(qrCodeData)
     })
-    .catch(error => res.send(error))
+    .catch((err) => {
+      console.log(err);
+    });
 }
