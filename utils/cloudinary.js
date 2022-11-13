1cf9e4b666ba159ffbb97f55e14c959469d14449
Emad Khatrush <qwe.emad@hotmail.com>
2022-11-13 13:51:32 +0300

    remove unused packages

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,41 +1,41 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const streamifier = require('streamifier');
+// const cloudinary = require('cloudinary').v2;
+// const { CloudinaryStorage } = require('multer-storage-cloudinary');
+// const streamifier = require('streamifier');
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
-// const storage = new CloudinaryStorage({
-//     cloudinary,
-//     params: {
-//       allowedFormats: ['jpeg', 'png', 'jpg']
-//     }
+// // const storage = new CloudinaryStorage({
+// //     cloudinary,
+// //     params: {
+// //       allowedFormats: ['jpeg', 'png', 'jpg']
+// //     }
+// //   });
+
+// let uploadFromBuffer = async (file, folderName) => {
+
+//   return new Promise((resolve, reject) => {
+
+//     let cld_upload_stream = cloudinary.uploader.upload_stream(
+//      {
+//        folder: folderName
+//      },
+//      (error, result) => {
+
+//        if (result) {
+//          resolve(result);
+//        } else {
+//          reject(error);
+//         }
+//       }
+//     );
+
+//     streamifier.createReadStream(file.buffer).pipe(cld_upload_stream);
 //   });
 
-let uploadFromBuffer = async (file, folderName) => {
+// };
 
-  return new Promise((resolve, reject) => {
-
-    let cld_upload_stream = cloudinary.uploader.upload_stream(
-     {
-       folder: folderName
-     },
-     (error, result) => {
-
-       if (result) {
-         resolve(result);
-       } else {
-         reject(error);
-        }
-      }
-    );
-
-    streamifier.createReadStream(file.buffer).pipe(cld_upload_stream);
-  });
-
-};
-
-module.exports = { cloudinary, uploadFromBuffer };
+// module.exports = { cloudinary, uploadFromBuffer };
