a18e9a6f0024a2aa8f9b620d3067fdca7be3d752
Emad Khatrush <qwe.emad@hotmail.com>
2022-12-24 18:07:08 +0300

    -

--- controllers/sendMessages.js
+++ controllers/sendMessages.js
@@ -6,25 +6,44 @@ module.exports.sendWhatsupMessage = async (req, res, next) => {
   const { phone, message, shouldVerifyQrCode } = req.body;
 
     // generete
+    // wbm
+    // .start({ sendMessage: !shouldVerifyQrCode })
+    // .then(async (qrCodeData) => {
+    //   if (shouldVerifyQrCode) {
+    //     res.status(200).send(qrCodeData);
+    //     wbm.waitQRCode();
+    //   } else {
+    //     const receiver = validatePhoneNumber(phone);
+    //     const phones = [receiver];
+    //     try {
+    //       await wbm.send(['905535728209'], message);
+    //       await wbm.end();
+    //     } catch (error) {
+    //       return next(new ErrorHandler(404, error.message));
+    //     }
+    //     res.status(200).send(qrCodeData)
+    //   }
+    // })
+    // .catch((err) => {
+    //   console.log("errrrrror", err);
+    // });
+
+    // generete
     wbm
-    .start({ sendMessage: !shouldVerifyQrCode })
+    .start({ qrCodeData: true, session: false, showBrowser: false })
     .then(async (qrCodeData) => {
-      if (shouldVerifyQrCode) {
-        res.status(200).send(qrCodeData);
-        wbm.waitQRCode();
-      } else {
+        res.send(qrCodeData);
+        await wbm.waitQRCode();
+
         const receiver = validatePhoneNumber(phone);
-        const phones = [receiver];
-        try {
-          await wbm.send(phones, message);
-          await wbm.end();
-        } catch (error) {
-          return next(new ErrorHandler(404, error.message));
-        }
+        console.log(receiver);
+        const phones = [phone];
+        await wbm.send(['905535728209'], message);
+        await wbm.end();
         res.status(200).send(qrCodeData)
-      }
     })
     .catch((err) => {
       console.log("errrrrror", err);
+      return next(new ErrorHandler(404, err.message));
     });
 }
